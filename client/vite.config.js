aa514c58e0fd3842ce68eea3e28a4048078ede3a
Nasso <narcissededome@gmail.com>
2026-02-05 16:40:12 +0100

    fff

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,4 +4,17 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    chunkSizeWarningLimit: 1000, // Augmenter la limite avant warning
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom'],
+          charts: ['recharts'],
+          icons: ['lucide-react'],
+          utils: ['xlsx', 'jspdf', 'date-fns'] // Si utilisés
+        }
+      }
+    }
+  }
 })
